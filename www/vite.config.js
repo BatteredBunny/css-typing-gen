4b4c44c4c6b894a67114caca7e2511311267c1a4
BatteredBunny <ayes2022@protonmail.com>
2022-11-05 01:25:40 +0200

    Changed to use vite pwa

--- www/vite.config.js
+++ www/vite.config.js
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite'
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
         topLevelAwait(),
         wasm(),
+        VitePWA({ 
+            registerType: 'autoUpdate',
+            manifest: {
+                name: "CSS Typing animation generator",
+                start_url: "https://css-gen.sly.ee",
+                short_name: "CSS Typing Gen",
+                description: "CSS Typing animation generator",
+                icons: [
+                    {
+                        src: "assets/android-chrome-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png"
+                    },
+                    {
+                        src: "assets/android-chrome-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png"
+                    }
+                ],
+                theme_color: "#1f1f1f",
+                background_color: "#0f0f0f",
+                display: "standalone"
+            }
+        }),
     ],
 });
