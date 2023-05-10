792d6850f76a58f464ade07af3157f6cb4e129c9
ayes-web <ayes2022@protonmail.com>
2023-05-10 03:52:24 +0300

    Cleaned code and improved README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
-use std::panic;
 use gloo::events::EventListener;
-use wasm_bindgen::{prelude::*, JsCast};
-use wasm_bindgen::JsValue;
+use std::panic;
 use std::rc::Rc;
+use wasm_bindgen::JsValue;
+use wasm_bindgen::{prelude::*, JsCast};
+use web_sys::Document;
 
 mod state;
 
-use crate::state::ApplicationState;
 use crate::state::Action;
+use crate::state::ApplicationState;
 
 #[wasm_bindgen]
 extern "C" {
@@ -24,153 +25,154 @@ extern "C" {
     fn normalize(code: &str, extra_settings: JsValue) -> String;
 }
 
+trait WrappedGetElementById {
+    fn wr_get_element_by_id<T: JsCast>(&self, id: &str) -> T;
+}
+
+impl WrappedGetElementById for Document {
+    fn wr_get_element_by_id<T: JsCast>(&self, id: &str) -> T {
+        self.get_element_by_id(id)
+            .expect("Can't find element with that ID!")
+            .dyn_into::<T>()
+            .expect("Wrong element present")
+    }
+}
+
 #[wasm_bindgen(start)]
 pub fn run() {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    let document = gloo::utils::document();
-
     let state: Rc<ApplicationState> = Rc::new(ApplicationState::new());
-    let state2 = Rc::clone(&state);
-    let state3 = Rc::clone(&state);
-    let state4 = Rc::clone(&state);
-    let state5 = Rc::clone(&state);
 
-    let generate_button = document
-        .get_element_by_id("generate-button")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlButtonElement>()
-        .unwrap();
-    let generate_button2 = generate_button.clone();
+    let inner_state = state.clone();
+    EventListener::new(
+        &state.elements.end_delay_seconds_input,
+        "input",
+        move |_| {
+            let mut new_length = inner_state
+                .elements
+                .end_delay_seconds_input
+                .value_as_number();
+            let previous_length = *inner_state.end_delay_seconds.borrow();
 
-    let main_input = document
-        .get_element_by_id("main-input")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlInputElement>()
-        .unwrap();
-    let main_input2 = main_input.clone();
+            if new_length.is_nan() {
+                // If invalid sets previous value
+                inner_state
+                    .elements
+                    .end_delay_seconds_input
+                    .set_value_as_number(previous_length);
+                return;
+            } else if new_length.is_sign_negative() {
+                // changes negative to positive
+                new_length = new_length.abs();
 
-    let generated_css_element = document.get_element_by_id("generated-css").unwrap();
+                if new_length == previous_length {
+                    return;
+                }
 
-    let typing_animation_style = document
-        .get_element_by_id("typing-animation-style")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlStyleElement>()
-        .unwrap();
+                inner_state
+                    .elements
+                    .end_delay_seconds_input
+                    .set_value_as_number(new_length);
+            }
 
-    let finished_state = document
-        .get_element_by_id("finished-state")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlDivElement>()
-        .unwrap();
+            *inner_state.end_delay_seconds.borrow_mut() = new_length;
 
-    let fix_interpolation_checkbox = document
-        .get_element_by_id("fix-interpolation")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlInputElement>()
-        .unwrap();
+            if inner_state.recording_start_time.borrow().is_some() {
+                let generated_css = match inner_state.generate_css() {
+                    Some(c) => c,
+                    None => return,
+                };
 
-    let wait_at_start_checkbox = document
-        .get_element_by_id("wait-at-start")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlInputElement>()
-        .unwrap();
-
-    let end_delay_seconds_input = Rc::new(document
-        .get_element_by_id("end-delay-seconds")
-        .unwrap()
-        .dyn_into::<web_sys::HtmlInputElement>()
-        .unwrap());
-    let end_delay_seconds_input2 = Rc::clone(&end_delay_seconds_input);
-
-    EventListener::new(&end_delay_seconds_input, "input", move |_| {
-        let mut new_length = end_delay_seconds_input2.value_as_number();
-        
-        if new_length.is_nan() { // If invalid sets previous value
-            end_delay_seconds_input2.set_value_as_number(*state5.end_delay_seconds.borrow());
-            return
-        } else if new_length.is_sign_negative() { // changes negative to positive
-            new_length = new_length.abs();
-            end_delay_seconds_input2.set_value_as_number(new_length);
-        } else {
-            *state5.end_delay_seconds.borrow_mut() = new_length;
-        }
-
-        if state5.recording_start_time.borrow().is_some() {
-            let generated_css = match state5.generate_css() {
-                Some(c) => c,
-                None => return,
-            };
-
-            state5.set_css(generated_css);
-        }
-    })
+                inner_state.set_css(generated_css);
+            }
+        },
+    )
     .forget();
 
-    EventListener::new(&wait_at_start_checkbox, "change", move |_| {
-        let mut toggle = state4.start_wait.borrow_mut();
-        *toggle = !*toggle;
-        drop(toggle);
+    let inner_state = state.clone();
+    EventListener::new(
+        &state.elements.wait_at_start_checkbox,
+        "change",
+        move |_| {
+            let mut toggle = inner_state.start_wait.borrow_mut();
+            *toggle = !*toggle;
+            drop(toggle);
 
-        if state4.recording_start_time.borrow().is_some() {
-            let generated_css = match state4.generate_css() {
-                Some(c) => c,
-                None => return,
-            };
+            if inner_state.recording_start_time.borrow().is_some() {
+                let generated_css = match inner_state.generate_css() {
+                    Some(c) => c,
+                    None => return,
+                };
 
-            state4.set_css(generated_css);
-        }
-    })
+                inner_state.set_css(generated_css);
+            }
+        },
+    )
     .forget();
 
-    EventListener::new(&fix_interpolation_checkbox, "change", move |_| {
-        let mut toggle = state3.fix_interpolation.borrow_mut();
-        *toggle = !*toggle;
-        drop(toggle);
+    let inner_state = state.clone();
+    EventListener::new(
+        &state.elements.fix_interpolation_checkbox,
+        "change",
+        move |_| {
+            let mut toggle = inner_state.fix_interpolation.borrow_mut();
+            *toggle = !*toggle;
+            drop(toggle);
 
-        if state3.recording_start_time.borrow().is_some() {
-            let generated_css = match state3.generate_css() {
-                Some(c) => c,
-                None => return,
-            };
+            if inner_state.recording_start_time.borrow().is_some() {
+                let generated_css = match inner_state.generate_css() {
+                    Some(c) => c,
+                    None => return,
+                };
 
-            state3.set_css(generated_css);
-        }
-    })
+                inner_state.set_css(generated_css);
+            }
+        },
+    )
     .forget();
 
     // Generate button click listener
-    EventListener::new(&generate_button, "click", move |_| {
-        let mut recording_started = state.recording_started.borrow_mut();
-
+    let inner_state = state.clone();
+    EventListener::new(&state.elements.generate_button, "click", move |_| {
+        let mut recording_started = inner_state.recording_started.borrow_mut();
         if *recording_started {
-            generate_button2.set_inner_html("Start recording");
+            inner_state
+                .elements
+                .generate_button
+                .set_inner_html("Start recording");
 
-            let generated_css = match state.generate_css() {
+            let generated_css = match inner_state.generate_css() {
                 Some(c) => c,
                 None => {
-                    main_input.set_hidden(true);
-                    finished_state.set_hidden(true);
+                    inner_state.elements.input.set_hidden(true);
+                    inner_state.elements.finished_state.set_hidden(true);
                     *recording_started = false;
                     return;
                 }
             };
 
-            main_input.set_hidden(true);
-            finished_state.set_hidden(false);
-            state.set_css(generated_css);
+            inner_state.elements.input.set_hidden(true);
+            inner_state.elements.finished_state.set_hidden(false);
+            inner_state.set_css(generated_css);
         } else {
-            typing_animation_style.set_inner_html("");
-            generated_css_element.set_inner_html("");
-            generate_button2.set_inner_html("Stop");
-            main_input.set_value("");
-            main_input.set_hidden(false);
-            finished_state.set_hidden(true);
+            inner_state
+                .elements
+                .typing_animation_style
+                .set_inner_html("");
+            inner_state
+                .elements
+                .generated_css_element
+                .set_inner_html("");
+            inner_state.elements.generate_button.set_inner_html("Stop");
+            inner_state.elements.input.set_value("");
+            inner_state.elements.input.set_hidden(false);
+            inner_state.elements.finished_state.set_hidden(true);
 
-            main_input.focus().unwrap();
+            inner_state.elements.input.focus().unwrap();
 
-            *state.recording_start_time.borrow_mut() = Some(chrono::Utc::now());
-            *state.recording_actions.borrow_mut() = Vec::new();
+            *inner_state.recording_start_time.borrow_mut() = Some(chrono::Utc::now());
+            *inner_state.recording_actions.borrow_mut() = Vec::new();
         }
 
         *recording_started = !*recording_started;
@@ -178,16 +180,12 @@ pub fn run() {
     .forget();
 
     // Input element listener!
-    EventListener::new(&main_input2, "input", move |event| {
-        if *state2.recording_started.borrow() {
-            state2.recording_actions.borrow_mut().push(Action {
+    let inner_state = state.clone();
+    EventListener::new(&state.elements.input, "input", move |_| {
+        if *inner_state.recording_started.borrow() {
+            inner_state.recording_actions.borrow_mut().push(Action {
                 date: chrono::Utc::now(),
-                value: event
-                    .target()
-                    .unwrap()
-                    .dyn_into::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
+                value: inner_state.elements.input.value(),
             });
         }
     })
