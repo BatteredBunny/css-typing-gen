10913f646c9f53fee18abac9cc788e32d1c8fff2
BatteredBunny <ayes2022@protonmail.com>
2023-12-03 05:27:39 +0200

    Bump deps

--- build.nix
+++ build.nix
@@ -1,60 +1,65 @@
-{ pkgs, rustPlatform, makeRustPlatform, mkYarnPackage, fetchYarnDeps }: let
-    targetName = "wasm32-unknown-unknown";
+{
+  pkgs,
+  makeRustPlatform,
+  mkYarnPackage,
+  fetchYarnDeps,
+}: let
+  targetName = "wasm32-unknown-unknown";
 
-    wasm-rust = pkgs.rust-bin.stable.latest.default.override {
-        extensions = [ "rust-src" ];
-        targets = [ targetName ];
-    };
+  wasm-rust = pkgs.rust-bin.stable.latest.default.override {
+    extensions = ["rust-src"];
+    targets = [targetName];
+  };
 
-    rustPlatformWasm = makeRustPlatform {
-        cargo = wasm-rust;
-        rustc = wasm-rust;
-    };
+  rustPlatformWasm = makeRustPlatform {
+    cargo = wasm-rust;
+    rustc = wasm-rust;
+  };
 
-    wasm-build = rustPlatformWasm.buildRustPackage rec {
-          name = "css-typing-gen";
-          cargoLock.lockFile = ./Cargo.lock;
+  wasm-build = rustPlatformWasm.buildRustPackage {
+    name = "css-typing-gen";
+    cargoLock.lockFile = ./Cargo.lock;
 
-          src = ./.;
+    src = ./.;
 
-          nativeBuildInputs = with pkgs; [
-            wasm-bindgen-cli
-          ];
+    nativeBuildInputs = with pkgs; [
+      wasm-bindgen-cli
+    ];
 
-          buildInputs = with pkgs; [
-            openssl
-            pkg-config
-            gnumake
-          ];
+    buildInputs = with pkgs; [
+      openssl
+      pkg-config
+      gnumake
+    ];
 
-          buildPhase = ''
-            cargo build --target ${targetName} --release
-            wasm-bindgen target/${targetName}/release/css_typing_gen.wasm --out-dir=$out/pkg
-          '';
+    buildPhase = ''
+      cargo build --target ${targetName} --release
+      wasm-bindgen target/${targetName}/release/css_typing_gen.wasm --out-dir=$out/pkg
+    '';
 
-          installPhase = "echo 'Skipping installPhase'";
-    };
+    installPhase = "echo 'Skipping installPhase'";
+  };
 in
-mkYarnPackage rec {
+  mkYarnPackage rec {
     src = ./www;
 
     offlineCache = fetchYarnDeps {
-        yarnLock = src + "/yarn.lock";
-        hash = "sha256-z7k/VVJ3SfFVbtybIrZR9Rjwq2awld/q0Njt0fXKGxQ=";
+      yarnLock = src + "/yarn.lock";
+      hash = "sha256-60NgkL1Pbw/zXrxLvtzoh1qPX54xPxX4w02Tdodz0I8=";
     };
 
     buildPhase = ''
-        ln -s ${wasm-build}/pkg ../pkg
-        export HOME=$(mktemp -d)
-        yarn --offline build
-        cp -r dist $out
+      ln -s ${wasm-build}/pkg ../pkg
+      export HOME=$(mktemp -d)
+      yarn --offline build
+      cp -r dist $out
     '';
 
     doDist = false;
 
     configurePhase = ''
-        ln -s $node_modules node_modules
+      ln -s $node_modules node_modules
     '';
 
     installPhase = "echo 'Skipping installPhase'";
-}
+  }
--- flake.nix
+++ flake.nix
@@ -1,35 +1,39 @@
 {
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
         wasm-rust = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [ "rust-src" ];
-            targets = [ "wasm32-unknown-unknown" ];
+          extensions = ["rust-src"];
+          targets = ["wasm32-unknown-unknown"];
         };
       in
-      with pkgs;
-      {
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            gnumake
-            wasm-rust
-            yarn
-            wasm-bindgen-cli
-          ];
-        };
-        packages.default = pkgs.callPackage ./build.nix { };
-      }
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              openssl
+              pkg-config
+              gnumake
+              wasm-rust
+              yarn
+              wasm-bindgen-cli
+            ];
+          };
+          packages.default = pkgs.callPackage ./build.nix {};
+        }
     );
 }
